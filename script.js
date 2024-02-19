44c3fda00f18b84555c876c9b14dc7a0b16c047c
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-02-19 19:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	
+	let date1inMS=Date.UTC();
+	let date2inMS=date2.getTime();
+
+	let differenceinMS = Math.abs(date2inMS-date1inMS);
+
+	let differenceinDays = Math.ceil(differenceinDays/(1000*3600*24));
+	return differenceinDays;
 };
 
-// Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// // Do not change the code below.
+const date1 = new Date.prompt("Enter Start Date.");
+const date2 = new Date.prompt("Enter End Date.");
+alert(dateDiffInDays(date1, date2));
+
+
